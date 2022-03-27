ce160708b0dae8417ccf5eaba7806657a455b4d5
HamzaCN <rajpoothamza923@gmail.com>
2022-03-27 21:56:49 +0500

    questions api updated

---
+++ src/api.js
@@ -0,0 +1,42 @@
+export const questions = [
+    {
+        id: 1,
+        question: 'Which character is pregnant at the beginning of The Grapes of Wrath?',
+        answer:'Rose of Sharon'
+    },
+    {
+        id: 2,
+        question: 'Who was the oldest person in the Bible, said to have lived 969 years?',
+        answer:'Methuselah'
+    },
+    {
+        id: 3,
+        question: 'What country is Puccini opera, Turandot, set in?',
+        answer:'China'
+    },
+    {
+        id: 4,
+        question: 'What literary work inspired Dream Theater "Pull Me Under" song?',
+        answer:'Hamlet'
+    },
+    {
+        id: 5,
+        question: 'What authors novel "East of Eden" was a symbolic recreation of the story of Cain and Abel?',
+        answer:'John Steinbeck'
+    },
+    {
+        id: 6,
+        question: 'Which character killed Cock Robin?',
+        answer:'The Sparrow'
+    },
+    {
+        id: 7,
+        question: 'What Chaucer collection of stories was sold by a group of pilgrims on their way to a shrine?',
+        answer:'Canterbury Tales'
+    },
+    {
+        id: 8,
+        question: 'In nautical folklore, which ship is condemned to sail the seas for all eternity?',
+        answer:'The Flying Dutchman'
+    }
+]
